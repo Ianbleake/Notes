35fab6fa62c8eed9a513f37c8bd3f140c9d3a9ab
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2024-08-02 14:06:54 -0600

    fix styles

--- notebook/src/main.jsx
+++ notebook/src/main.jsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import './styles/index.css'
-//import './styles/notes.css'
+import './styles/notes.css'
 
 const root = document.getElementById("root")
 ReactDOM.createRoot(root).render(<App />);
