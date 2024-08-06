2edffc1ffdefec6696a2c42a53bd2ae9b1619705
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2024-08-06 13:49:04 -0600

    add firebase

--- notebook/src/services/notes.js
+++ notebook/src/services/notes.js
@@ -1,19 +1,25 @@
-import axios from 'axios'
-const baseUrl = 'http://localhost:3001/notes'
+import { db } from '../firebase/config';
+import { collection, getDocs, addDoc, updateDoc, doc, getDoc } from 'firebase/firestore';
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+const notesCollection = collection(db, 'notes');
 
-const create = newObject => {
-  const request = axios.post(baseUrl, newObject)
-  return request.then(response => response.data)
-}
+const getAll = async () => {
+  const snapshot = await getDocs(notesCollection);
+  const notes = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  return notes;
+};
 
-const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then(response => response.data)
-}
+const create = async (newObject) => {
+  const docRef = await addDoc(notesCollection, newObject);
+  const docSnap = await getDoc(docRef);
+  return { id: docRef.id, ...docSnap.data() };
+};
 
-export default {  getAll, create, update }
+const update = async (id, newObject) => {
+  const noteDoc = doc(db, 'notes', id);
+  await updateDoc(noteDoc, newObject);
+  const updatedDoc = await getDoc(noteDoc);
+  return { id: updatedDoc.id, ...updatedDoc.data() };
+};
+
+export default { getAll, create, update };
